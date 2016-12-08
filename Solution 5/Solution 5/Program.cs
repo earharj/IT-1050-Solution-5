0f27cf833a5d38b596db79496b325e3b8cc0f490
Jackie Earhart <jmricha13@yahoo.com>
2016-12-07 23:06:47 -0500

    Solution 5

--- Solution 5/Solution 5/Program.cs
+++ Solution 5/Solution 5/Program.cs
@@ -47,27 +47,34 @@ namespace Solution_5
                 System.Console.WriteLine("Enter Destination Y Coordinate Between 0 and 7:");
                 dy = int.Parse(System.Console.ReadLine());
                 System.Console.Clear();
-                for (int i = 0; i < 8; i++)
 
+                if (tx <= 7 && tx >= 0 && ty <= 7 && ty >= 0 && dx <= 7 && dx >= 0 && dy <= 7 && dy >= 0)
                 {
-                    System.Console.WriteLine("+---+---+---+---+---+---+---+---+");
-                    board[i] = new string[8];
+                    for (int i = 0; i < 8; i++)
 
-                    for (int j = 0; j < 8; j++)
                     {
-                        board[i][j] = "  ";
-                        if (i <= 1 || i >= 6)
+                        System.Console.WriteLine("+---+---+---+---+---+---+---+---+");
+                        board[i] = new string[8];
+
+                        for (int j = 0; j < 8; j++)
                         {
-                            board[i][j] = "X ";
+                            board[i][j] = "  ";
+                            if (i <= 1 || i >= 6)
+                            {
+                                board[i][j] = "X ";
+                            }
+                            board[tx][ty] = "  ";
+                            board[dx][dy] = "X ";
+                            System.Console.Write("| " + board[i][j]);
                         }
-                        board[tx][ty] = "  ";
-                        board[dx][dy] = "X ";
-                        System.Console.Write("| " + board[i][j]);
-                     }
-                    System.Console.WriteLine("|");
+                        System.Console.WriteLine("|");
+                    }
+                    System.Console.WriteLine("+---+---+---+---+---+---+---+---+");
                 }
-                System.Console.WriteLine("+---+---+---+---+---+---+---+---+");
-            }                                      
+            }
+            System.Console.WriteLine("You entered a value that is out of bounds.");
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();                                  
         }
     }
 }
